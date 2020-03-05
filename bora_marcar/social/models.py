2d1a2248bcad3a346b76b3a88148c57eb7f3bfbe
arthur mario <artmario@gmail.com>
2020-03-05 19:49:25 -0400

    modelo base do evento
    segue os campos iniciais de um evento a ser votado

--- bora_marcar/social/models.py
+++ bora_marcar/social/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Event(models.Model):
+  id = models.IntegerField()
+  nome = models.CharField(max_length=200)
+  desc = models.TextField()
+  type = models.CharField(max_length=200)
+  inicio = models.DateTimeField(blank=True, null=True)
+  fim = models.DateTimeField(blank=True, null=True)
+  range  = models.CharField(max_length=200)
+  created_date = models.DateTimeField(default=timezone.now)
+  end_poll = models.DateTimeField(blank=True, null=True)
+  status = models.DateTimeField(blank=True, null=True)
+  id_admin = models.IntegerField()
